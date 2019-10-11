01c27bc09e851374aa6f166667ceb2f9f8440669
Marissa <marissa.wagner@littles.moe>
2019-10-11 12:18:44 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# telegram-user-bot
+
+Silly private user-bot. Is able to randomly owofy some messages. Has an @all-Command and has a stupid . _ . animation ._.
---
+++ bot.py
@@ -0,0 +1,74 @@
+import sys
+import re
+import random
+from random import random
+import asyncio
+from asyncio import sleep
+from telethon import *
+from telethon.tl.functions.messages import GetFullChatRequest
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+
+# Import settings
+import env
+
+########################
+
+def probability(percent):
+	outcome = random() < percent
+	return outcome
+
+# parse account from cli
+if len(sys.argv) != 3:
+	print("You need to specify an account name and a phone number!")
+	exit()
+
+account_file = sys.argv[1]
+telephone_number = sys.argv[2]
+	
+client = TelegramClient(account_file, env.api_id, env.api_hash)
+
+@client.on(events.NewMessage(outgoing=True))
+async def self(event):
+	message_string = event.text
+
+	if "@all" in message_string:
+		await event.delete()
+		all_participants = await client.get_participants(event.message.chat_id)
+		mention_string = "[@all](tg://need_update_for_some_feature)"
+		for user in all_participants:
+			mention_string += "[⁣](tg://user?id=" + str(user.id) + ")"
+		message_string = message_string.replace("@all", mention_string)
+		await client.send_message(event.message.chat_id, message_string)
+		return
+
+	if "o" in message_string or "u" in message_string:
+		no_urls = re.sub(r'[^ ]+\.[^ ]+', "", message_string)
+		input_list = list(no_urls)
+		for i, c in enumerate(input_list):
+			if c == "o" and probability(0.002):
+				input_list[i] = "owo"
+			if c == "u" and probability(0.002):
+				input_list[i] = "uwu"
+		output = "".join(input_list)
+		message_string = output
+
+	if message_string != event.text:
+		await event.edit(message_string)
+
+	if ".. _ ." in message_string or "..-." in message_string:
+		str_orig = message_string
+		new_message = str_orig.replace(".. _ .", ". _ .").replace("..-.", ".-.")
+		await event.edit(new_message)
+		for _ in range(10):               
+			await sleep(0.5)
+			await event.edit(str_orig.replace(".. _ .", "._  .").replace("..-.", "._."))
+			await sleep(0.5)
+			await event.edit(str_orig.replace(".. _ .", ". _ .").replace("..-.", ".-."))
+			await sleep(0.5)
+			await event.edit(str_orig.replace(".. _ .", ".  _.").replace("..-.", "._."))
+			await sleep(0.5)
+			await event.edit(str_orig.replace(".. _ .", ". _ .").replace("..-.", ".-."))
+						
+client.start(phone=telephone_number)
+client.run_until_disconnected()
