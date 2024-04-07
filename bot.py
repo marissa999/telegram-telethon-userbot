cbce9dafa9adcb2860a83af3a7ad60b097939179
marissa999 <github@littles.moe>
2024-04-07 15:36:08 +0200

    add ;_; animation

--- bot.py
+++ bot.py
@@ -12,7 +12,14 @@ from telethon.tl.types import ChannelParticipantsSearch
 # Import settings
 import env
 
-########################
+
+# Frames for animations
+animation = [
+	[ ".. _ .",		". _ .",	"._  .",	". _ .",	".  _."],
+	[ "..-.",		".-.",		"._.",		".-.",		"._."],
+	[ ".p.p",		"p.p",		"q.q",		"p.p",		"q.q"],
+	[ ".;_;",		";_;",		"⁏_⁏",		";_;",		"⁏_⁏"]
+]
 
 # parse account from cli
 if len(sys.argv) != 3:
@@ -40,17 +47,42 @@ async def self(event):
 
 	if ".. _ ." in message_string or "..-." in message_string or ".p.p" in message_string:
 		str_orig = message_string
-		new_message = str_orig.replace(".. _ .", ". _ .").replace("..-.", ".–.").replace(".p.p", "p.p")
+		new_message = (str_orig
+			.replace(".. _ .", ". _ .")
+			.replace("..-.", ".–.")
+			.replace(".p.p", "p.p")
+			.replace(".;_;", ";_;")
+		)
 		await event.edit(new_message)
 		for _ in range(20):			   
 			await sleep(0.5)
-			await event.edit(str_orig.replace(".. _ .", "._  .").replace("..-.", "._.").replace(".p.p", "q.q"))
+			await event.edit(str_orig
+				.replace(".. _ .", "._  .")
+				.replace("..-.", "._.")
+				.replace(".p.p", "q.q")
+				.replace(".;_;", "⁏_⁏")
+			)
 			await sleep(0.5)
-			await event.edit(str_orig.replace(".. _ .", ". _ .").replace("..-.", ".–.").replace(".p.p", "p.p"))
+			await event.edit(str_orig
+				.replace(".. _ .", ". _ .")
+				.replace("..-.", ".–.")
+				.replace(".p.p", "p.p")
+				.replace(".;_;", ";_;")
+			)
 			await sleep(0.5)
-			await event.edit(str_orig.replace(".. _ .", ".  _.").replace("..-.", "._.").replace(".p.p", "q.q"))
+			await event.edit(str_orig
+				.replace(".. _ .", ".  _.")
+				.replace("..-.", "._.")
+				.replace(".p.p", "q.q")
+				.replace(".;_;", "⁏_⁏")
+			)
 			await sleep(0.5)
-			await event.edit(str_orig.replace(".. _ .", ". _ .").replace("..-.", ".–.").replace(".p.p", "p.p"))
+			await event.edit(str_orig
+				.replace(".. _ .", ". _ .")
+				.replace("..-.", ".–.")
+				.replace(".p.p", "p.p")
+				.replace(".;_;", ";_;")
+			)
 
 client.start(phone=telephone_number)
 client.run_until_disconnected()
